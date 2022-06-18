0cda92079a30ee689a0a1d73dd6cc73b934fe6fa
Ben Wishoshavich <ben@celcyon.com>
2022-06-18 11:10:51 -0700

    Setup migrations to match schema

---
+++ prisma/migrations/20220618180948_posts/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "slug" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "markdown" TEXT NOT NULL,
+    "excerpt" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
